0f6ef1b162f184f421b380c250dba16333491e37
JAndresLM <josandlopmol@hotmail.com>
2016-07-04 17:35:29 -0600

    Se agregó la cédula del cliente

--- models/asociados.py
+++ models/asociados.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 from openerp import fields, models
 
 class Partner(models.Model):
     _inherit = 'res.partner'
     asociado = fields.Boolean("Asociado", default=False)
     actividades_ids= fields.One2many('aso.actividades_por_asociado','asociado_id','Actividades del Asociado')
+    cedula = fields.Char(string="Cédula")
