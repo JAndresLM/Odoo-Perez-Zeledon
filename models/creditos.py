a0155ba502b8b91681f4e8eee01e74dc8d190d91
JAndresLM <josandlopmol@hotmail.com>
2016-07-10 17:58:58 -0600

    Créditos, estados correctos pero unidos

--- models/creditos.py
+++ models/creditos.py
@@ -8,15 +8,54 @@ class Creditos(models.Model):
     name = fields.Char(string="N° de Crédito", required=True)
     asociado_id = fields.Many2one('res.partner', string="Asociado", required=True, domain=[('asociado', '=', True)])
     fuente_id = fields.Many2one('aso.fuentes', string="Fuente")
-    fecha = fields.Datetime(string="Fecha de solicitud", default=fields.Datetime.now)
+    fecha_solicitud = fields.Date(string="Fecha de solicitud", default=fields.Datetime.now)
     actividad= fields.Many2one('aso.actividades_por_asociado',string="Actividad", domain=[('asociado_id', '=', 0)])
+    solicitud_c1= fields.Char(string="N° Solicitud C1")
+    cosecha = fields.Many2one('aso.cosechas',string="Cosecha")
+    monto = fields.Float(string="Monto")
+    plazo = fields.Integer(string="Plazo (meses)")
+    pagare_vigente= fields.Boolean(string="Pagaré Vigente")
+    fecha_aprobacion = fields.Date(string="Fecha de Aprobación")
+    fecha_vencimiento = fields.Date(string="Fecha de Vencimiento")
+    observaciones = fields.Text(string="Observaciones")
+    state=fields.Selection([
+        ('r', "Rechazado"),
+        ('ti', "Trámite Inicial"),
+        ('a', "Aprobado"),
+        ('eu', "Enviado a UPIAV"),
+        ('du', "Desembolsado por UPIAV"),
+        ('f', "Formalizado"),
+        ('pp', "Pago Parcial"),
+        ('c', "Cancelado"),],default='ti',string="Estado")
 
+    @api.multi
+    def action_tramite_inicial(self):
+    	self.state='ti'
+    @api.multi
+    def action_aprobado(self):
+    	self.state='a'
+    @api.multi
+    def action_enviado(self):
+    	self.state='eu'
+    @api.multi
+    def action_desembolsado(self):
+    	self.state='du'
+    @api.multi
+    def action_formalizado(self):
+    	self.state='f'
+    @api.multi
+    def action_pago_parcial(self):
+    	self.state='pp'
+    @api.multi
+    def action_cancelado(self):
+    	self.state='c'
+    @api.multi
+    def action_rechazado(self):
+    	self.state='r'
     @api.onchange('asociado_id')
     def _get_actividades(self):
-        self.actividad = self.env['aso.actividades_por_asociado']
-        vals = {}
-        if self.asociado_id:
-            vals['domain'] = {
-                'actividad': [('asociado_id', '=', self.asociado_id.id)]
-            }
-        return vals
+		self.actividad = self.env['aso.actividades_por_asociado']
+		vals = {}
+		if self.asociado_id:
+			vals['domain'] = {'actividad':[('asociado_id','=',self.asociado_id.id)]}
+		return vals
