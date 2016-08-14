780f602dadce6042ae2de1ba91f4cd0aa7599454
JAndresLM <josandlopmol@hotmail.com>
2016-08-14 13:47:21 -0600

    Se agragaron los totales, pero dan errores

--- models/pesos_lines.py
+++ models/pesos_lines.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields
+from openerp import models, fields, api
 
 class Pesos_lines(models.Model):
 	_name = 'aso.pesos_lines'
@@ -9,8 +9,29 @@ class Pesos_lines(models.Model):
 	humedad = fields.Float(string="Humedad (Kg)")
 	subtotal=fields.Float(string='Subtotal (Kg)')
 	por_dano = fields.Float(string='Daño (%)')
-	merma_dano = fields.Float(string='Merma (Kg)')
-	por_humedad = fields.Float(string='Humedad (%)')
-	merma_humedad = fields.Float(string='Merma (Kg)')
-	total_kg = fields.Float(string='Total Kg')
-	total_qq = fields.Float(string='Total QQ')
+	merma_dano = fields.Float(string='Merma (Kg)', readonly=True, compute='_calcular_merma_dano')
+	por_humedad = fields.Float(string='Humedad (%)', readonly=True, compute='_calcular_por_humedad')
+	merma_humedad = fields.Float(string='Merma (Kg)', readonly=True, compute='_calcular_merma_humedad')
+	total_kg = fields.Float(string='Total (Kg)', readonly=True, compute='_calcular_total_line')
+
+	@api.depends('subtotal','por_dano')
+	def calcular_merma_dano(self):
+		for r in self:
+			r.merma_dano = subtotal * por_dano
+
+	@api.depends('subtotal','humedad')
+	def _calcular_por_humedad(self):
+		for r in self:
+			r.por_humedad = 0.0
+			if r.humedad > 16:
+				r.por_humedad = r.humedad - 16
+
+	@api.depends('subtotal','por_humedad')
+	def _calcular_merma_humedad(self):
+		for r in self:
+			r.merma_humedad = subtotal * por_humedad
+
+	@api.depends('subtotal','merma_humedad','merma_dano')
+	def _calcular_total_line(self):
+		for r in self:
+			r.total_kg = subtotal - merma_humedad - merma_dano
